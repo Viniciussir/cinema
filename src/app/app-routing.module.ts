c8917861a0480b768c97d06639d659c69d3a851e
Vinicius Scampini <viniciuscscampini@gmail.com>
2023-05-21 21:30:54 -0300

    login module creation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './signin/signin/signin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: SigninComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
