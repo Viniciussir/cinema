ca291cde1f909f7689b01db8eb2b8c2985cc7f6c
Vinicius Scampini <viniciuscscampini@gmail.com>
2023-06-19 23:22:02 -0300

    Cities service call

--- src/app/register/servico/register.service.ts
+++ src/app/register/servico/register.service.ts
@@ -2,12 +2,15 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Register } from "./register";
 import { RegisterDados } from "./registerdados";
+import { Observable } from "rxjs";
 
 @Injectable({
     providedIn:'root'
 })
 
 export class RegisterService {
+
+    private apiUrl = 'https://servicodados.ibge.gov.br/api/v1/localidades';
 
     constructor(
         private http: HttpClient
@@ -16,6 +19,11 @@ export class RegisterService {
     gravarDadosCadastro(registerDados:RegisterDados) {
         return this.http.post("https://api.github.com/repos/OWNER/REPO", 
         {'oFiltro': JSON.stringify(registerDados)})
+    }
+
+    getCidades(estado: string): Observable<any[]> {
+        const url = `${this.apiUrl}/estados/${estado}/municipios`;
+        return this.http.get<any[]>(url);
     }
 
 }
